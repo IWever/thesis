2c62a58dc0fea79833b62ef6b22a7ef930e5760f
Ingmar Wever <i.wever@student.tudelft.nl>
2018-03-06 11:56:54 +0100

    Defined methods for next version of application, and kept playing with GUI-test

--- Model/Python/GUI-test.py
+++ Model/Python/GUI-test.py
@@ -15,6 +15,35 @@ vesselNames = StringVar(value=vessels)
 def getPosition(event):
     print(event.x, event.y)
 
+def motion(event):
+    # calculate current angle relative to initial angle
+    theta = slider2.get() * math.pi / 180
+
+    shipA.heading = math.fmod(shipA.heading + slider2.get(), 360)
+
+    print(map.coords(shipA.shipPlot))
+    print(type(map.coords(shipA.shipPlot)))
+
+    ox = map.coords(shipA.shipPlot)[0]
+    oy = map.coords(shipA.shipPlot)[1]
+
+    newxy = []
+    for i in range(0, int(len(map.coords(shipA.shipPlot))/2)):
+        px = map.coords(shipA.shipPlot)[i * 2]
+        py = map.coords(shipA.shipPlot)[i * 2 + 1]
+
+        newx = math.cos(theta) * (px - ox) - math.sin(theta) * (py - oy) + ox
+        newy = math.sin(theta) * (px - ox) + math.cos(theta) * (py - oy) + oy
+
+        newxy.append(newx)
+        newxy.append(newy)
+    map.coords(shipA.shipPlot, *newxy)
+
+    slider2.set(0)
+
+def itemSelected(event):
+    if len(vesselList.curselection()) is 1:
+        print("%s selected" % vesselList.curselection())
 
 
 # Use objects
@@ -24,16 +53,16 @@ class Ship:
         self.width = width
         self.map = map
         self.xy = [(.5 * self.width, 1.5 * self.length), (self.width, self.length), (self.width, 0), (0, 0), (0, self.length)]
+        self.heading = 0
 
         self.shipPlot = map.create_polygon(self.xy)
         self.map.move(self.shipPlot, 250, 250)
 
 
     def moveShip(self):
-        dx = slider1.get()
-        dy = slider1.get()
+        v = slider1.get()
 
-        self.map.move(self.shipPlot, dx, dy)
+        self.map.move(self.shipPlot, -v * math.sin(self.heading * math.pi / 180), v * math.cos(self.heading * math.pi / 180))
         self.map.after(100, self.moveShip)
 
 
@@ -43,7 +72,7 @@ class Ship:
 
 ## Info-bar at the bottom
 info = StringVar()
-info.set("select a vessel to add")
+info.set("Let it sail")
 
 infoBar = Frame(root, bg='white')
 infoBar.pack(side=BOTTOM, fill=X, expand=FALSE)
@@ -68,6 +97,7 @@ vesselList = Listbox(optionMenu)
 vesselList.pack(side=BOTTOM, fill=BOTH)
 for vessel in vessels:
     vesselList.insert(END, vessel)
+vesselList.bind('<<ListboxSelect>>', itemSelected)
 
 slider1 = Scale(optionMenu, from_=-10, to=10, orient=HORIZONTAL)
 slider1.pack(side=TOP, fill=X)
@@ -75,34 +105,13 @@ slider2 = Scale(optionMenu, from_=-360, to=360, orient=HORIZONTAL)
 slider2.pack(side=TOP, fill=X)
 
 slider1.bind("<ButtonRelease-1>", shipA.moveShip())
-slider2.bind("<ButtonRelease-1>", shipA.moveShip())
+slider2.bind("<ButtonRelease-1>", motion)
 
 ## Rotating ship
 center = 250, 250
 
-def motion(event):
-    # calculate current angle relative to initial angle
-    theta = slider2.get() * math.pi / 180
-
-    ox = map.coords(shipA.shipPlot)[0]
-    oy = map.coords(shipA.shipPlot)[1]
-
-    newxy = []
-    for i in range(0, int(len(map.coords(shipA.shipPlot))/2)):
-        px = map.coords(shipA.shipPlot)[i * 2]
-        py = map.coords(shipA.shipPlot)[i * 2 + 1]
-
-        newx = math.cos(theta) * (px - ox) - math.sin(theta) * (py - oy) + ox
-        newy = math.sin(theta) * (px - ox) + math.cos(theta) * (py - oy) + oy
-
-        newxy.append(newx)
-        newxy.append(newy)
-    map.coords(shipA.shipPlot, *newxy)
-
-    slider2.set(0)
 
 
-slider2.bind("<ButtonRelease-1>", motion)
 
 
 root.mainloop()
